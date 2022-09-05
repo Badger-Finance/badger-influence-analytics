cb88c76adff235ed01ae5e72e1febac51f2a3dce
SHAKOTN <shakotn@protonmail.com>
2022-09-05 19:35:56 +0300

    feat: consider fees in table reporting

--- notebooks/bribes_research/constants.py
+++ notebooks/bribes_research/constants.py
@@ -1,7 +1,7 @@
 BRIBES_URL = "https://api.llama.airforce/bribes"
 DASHBOARD_URL = "https://api.llama.airforce//dashboard"
 TABLE_COLUMNS = [
-    "Protocol", "BADGER total bribe in $", "Fees",
+    "Protocol", "BADGER bribe", "Total bribe in $", "Fees",
     "Bribes Portion %", "Bribes Portion", "Dilution", "$I/O"
 ]
 PROTOCOL_CVX = "CVX"
