9c875c914b13428b0f723d8b1afa1333eadcc5d3
SHAKOTN <shakotn@protonmail.com>
2022-09-05 20:43:56 +0300

    feat: impl simple chart for average $I/O

--- notebooks/bribes_research/constants.py
+++ notebooks/bribes_research/constants.py
@@ -1,8 +1,11 @@
 BRIBES_URL = "https://api.llama.airforce/bribes"
 DASHBOARD_URL = "https://api.llama.airforce//dashboard"
-TABLE_COLUMNS = [
+TABLE_COLUMNS_COMMON_METRICS = [
     "Protocol", "BADGER bribe", "Total bribe in $", "Fees",
     "Bribes Portion %", "Bribes Portion", "Dilution", "$I/O"
+]
+TABLE_AW_METRIC_HEADERS = [
+    "BADGER Bribe", "Total bribe in $", "Average $I/O",
 ]
 PROTOCOL_CVX = "CVX"
 PROTOCOL_AURA = "AURA"
