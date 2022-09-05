259ee24d78f054d91af76e54e0a6367eda951466
SHAKOTN <shakotn@protonmail.com>
2022-09-05 18:39:13 +0300

    chore: refactor calculations a bit

---
+++ notebooks/bribes_research/constants.py
@@ -0,0 +1,7 @@
+BRIBES_URL = "https://api.llama.airforce/bribes"
+DASHBOARD_URL = "https://api.llama.airforce//dashboard"
+TABLE_COLUMNS = [
+    "Protocol", "BADGER total bribe in $", "Bribes Portion %", "Bribes Portion", "Dilution", "$I/O"
+]
+PROTOCOL_CVX = "CVX"
+PROTOCOL_AURA = "AURA"
