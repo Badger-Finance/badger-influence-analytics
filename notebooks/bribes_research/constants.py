e067801ccce79bef3bc86bbbd94765efe35dc93a
SHAKOTN <shakotn@protonmail.com>
2022-09-07 15:45:38 +0300

    feat: add new $I/O metrics with different pool weights

--- notebooks/bribes_research/constants.py
+++ notebooks/bribes_research/constants.py
@@ -1,8 +1,9 @@
 BRIBES_URL = "https://api.llama.airforce/bribes"
 DASHBOARD_URL = "https://api.llama.airforce//dashboard"
 TABLE_COLUMNS_COMMON_METRICS = [
-    "Protocol", "BADGER bribe", "Total bribe in $", "Fees",
-    "Bribes Portion %", "Bribes Portion", "Dilution", "$I/O"
+    "Market", "BADGER bribe", "Total bribe in $", "Fees",
+    "Bribes Portion %", "Bribes Portion", "Dilution",
+    "$I/O 50%", "$I/O 60%", "$I/O 80%", "$I/O 100%"
 ]
 TABLE_AW_METRIC_HEADERS = [
     "BADGER Bribe", "Total bribe in $", "Average $I/O",
