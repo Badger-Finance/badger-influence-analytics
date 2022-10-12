eb29c4fe876a09cf9e92a3fe58c526245919c00b
SHAKOTN <shakotn@protonmail.com>
2022-10-12 16:46:37 +0300

    feat: WIP on fees MIM

--- notebooks/ebtc_research/generate_data.py
+++ notebooks/ebtc_research/generate_data.py
@@ -8,20 +8,23 @@ MINIMAL_FEE = 0.005  # in %
 MAX_FEE = 0.05  # in %
 # Starting with 10m volume
 SIMULATED_VOLUME = 1000000
+MIM_LEVERAGES = [2, 3, 4, 5, 6]
+LEVERAGE_PROBABILITIES = [50, 40, 30, 20, 10, 1]
 
 
-def _generate_probabilities() -> Tuple[List, List]:
+def _generate_probabilities(
+        initial_prob=50, minimal_value=MINIMAL_FEE, max_value=MAX_FEE,
+) -> Tuple[List, List]:
     """
     Generates two tables:
     1. Fees values with probability_step_down variable down
     2. Fees probabilities, the lower the fee - the higher probability of fee to appear
     """
-    initial_probability = 50
     probability_step_down = 4
     possible_fees = []
     probabilities = []
-    probability = initial_probability
-    for fee in np.arange(MINIMAL_FEE, MAX_FEE, 0.001):
+    probability = initial_prob
+    for fee in np.arange(minimal_value, max_value, 0.001):
         possible_fees.append(fee)
         probabilities.append(probability)
         if not probability <= probability_step_down:
@@ -60,3 +63,42 @@ def get_tables() -> Tuple[List, List, List, List, List]:
         revenue_table_randomized_fees,
         revenue_table_randomized_fees_double,
         volumes)
+
+
+def get_tables_mimlike() -> Tuple[List, List]:
+    """
+    Simulated fees capture on MIM-like protocol
+    """
+    fees_capture = 0.3  # in %
+    min_fee = 0.005
+    volumes = []
+    for volume_multiplier in range(1, 16):
+        volumes.append(SIMULATED_VOLUME * volume_multiplier)
+
+    revenue_table = []
+    revenue_table_fees_leverage = []
+    for sim_volume in volumes:
+        revenue_table.append(
+            [
+                # Volume
+                "{:10.4f}".format(sim_volume),
+                # Treasury capture
+                f"{fees_capture * 100}%",
+                # Revenue from fees + capture %
+                (sim_volume * min_fee) * fees_capture,
+                # Fee itself
+                f"{min_fee * 100}%",
+                # Leverage
+                1,
+            ]
+        )
+        for leverage in MIM_LEVERAGES:
+            revenue_table.append(
+                [
+                    "{:10.4f}".format(sim_volume),
+                    f"{fees_capture * 100}%",
+                    (sim_volume * leverage) * min_fee,
+                    f"{min_fee * 100}%",
+                    leverage
+                ])
+    return revenue_table, volumes
