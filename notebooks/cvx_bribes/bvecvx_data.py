950d6e21e30e97b9c051add5bb8784c9327779ed
SHAKOTN <shakotn@protonmail.com>
2023-05-04 10:25:28 +0300

    chore: new bvecvx round

--- notebooks/cvx_bribes/bvecvx_data.py
+++ notebooks/cvx_bribes/bvecvx_data.py
@@ -311,9 +311,23 @@ VOTING_DATA_R42 = {
     'llama_vlcvx': Decimal('0.05899'),
 }
 
+VOTING_DATA_R43 = {
+    'round': '27th Apr 2023',
+    'total_cvx_sold_in_$': Decimal('59040.3'),
+    'total_badger_sold_in_$': Decimal('22551.23'),
+    'amount_cvx': Decimal('11592.44'),
+    'amount_badger': Decimal('8097.4'),
+    'badger_price_at_the_moment_of_sale_in_$': Decimal('2.79'),
+    'cvx_price_at_the_moment_of_sale_in_$': Decimal('5.1'),
+    '$/vlCVX for total votes': Decimal('0.043'),
+    '$/vlCVX without badgerwbtc': Decimal('0.0472'),
+    'llama_vlcvx': Decimal('0.04482'),
+}
+
 VOTING_DATASETS = [
     VOTING_DATA_R23, VOTING_DATA_R24, VOTING_DATA_R25, VOTING_DATA_R26, VOTING_DATA_R27,
     VOTING_DATA_R28, VOTING_DATA_R29, VOTING_DATA_R30, VOTING_DATA_R31, VOTING_DATA_R32,
     VOTING_DATA_R33, VOTING_DATA_R34, VOTING_DATA_R35, VOTING_DATA_R36, VOTING_DATA_R37,
-    VOTING_DATA_R38, VOTING_DATA_R39, VOTING_DATA_R40, VOTING_DATA_R41, VOTING_DATA_R42
+    VOTING_DATA_R38, VOTING_DATA_R39, VOTING_DATA_R40, VOTING_DATA_R41, VOTING_DATA_R42,
+    VOTING_DATA_R43,
 ]
